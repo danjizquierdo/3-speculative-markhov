a4656b2271e9b62087af377aeece16351de94b28
Dan Izquierdo <dan.j.izquierdo@gmail.com>
2019-03-05 18:24:26 -0500

    Cleanup functionality

--- ScriptWhichMustNotBeNamed.py
+++ ScriptWhichMustNotBeNamed.py
@@ -1,17 +1,18 @@
-#!/usr/bin/python
-
+#!/anaconda3/bin/python
 import subprocess
 import random
 import re
 import os
+import sys
 import time
 from twython import Twython
 from raise_corpus import Necromancer
 
-with open('./keys.txt') as f:
+print('Opening keys')
+with open('/Users/flatironschool/blogs/3-speculative-markhov/keys.txt') as f:
     content=f.readlines()
 keys= [line.strip() for line in content]
-
+print('Opened keys')
 # input API key, API secret key, Access token, Secret access token
 
 twitter = Twython(keys[0],
@@ -20,22 +21,25 @@ twitter = Twython(keys[0],
                   keys[3])
 
 def output_tweet(text,tweeted):
-    if tweeted==0:
-        twitter.update_status(status=text)
-    else:
-        last_tweet=twitter.get_home_timeline(count=1)
-        twitter.update_status(status=text,in_reply_to_status_id=last_tweet[0]['id'])
+	print('tweeting')
+	if tweeted==0:
+		twitter.update_status(status=text)
+	else:
+		last_tweet=twitter.get_home_timeline(count=1)
+		twitter.update_status(status=text,in_reply_to_status_id=last_tweet[0]['id'])
 
 # generate title and description
+print('Rising from the dead')
 tweet = Necromancer().shambler('title',6)
 tweet+= ': '+Necromancer().shambler('description',60)
+print('Risen')
 dregs=''
 tweeted=0
 while len(tweet) > 280:
     # if it is too long, try to cut off at a sentence
     try:
-        dregs= re.search("(.+?\.)(.*)", tweet).group(2)
-        tweet= re.search("(.+?\.)(.*)", tweet).group(1)
+        dregs= re.search(r"(.+?\.)(.*)", tweet).group(2)
+        tweet= re.search(r"(.+?\.)(.*)", tweet).group(1)
         if len(tweet) >280:
             # at least we tried, just take the first 140 characters
             dregs=tweet[280:]+dregs
--- raise_corpus.py
+++ raise_corpus.py
@@ -1,5 +1,8 @@
+
+#!/usr/bin/python
 import pandas as pd
 import re
+import sys
 import numpy as np
 
 class Necromancer():
@@ -12,9 +15,9 @@ class Necromancer():
             yield (corpus[word],corpus[word+1])
 
     def raise_corpus(self,medium):
-        df1=pd.read_csv(f'./csvs/fantasy-{medium}.csv')
-        df2=pd.read_csv(f'./csvs/sci-fi-{medium}.csv')
-        df3=pd.read_csv(f'./csvs/speculative-{medium}.csv')
+        df1=pd.read_csv(f'/Users/flatironschool/blogs/3-speculative-markhov/csvs/fantasy-{medium}.csv')
+        df2=pd.read_csv(f'/Users/flatironschool/blogs/3-speculative-markhov/csvs/sci-fi-{medium}.csv')
+        df3=pd.read_csv(f'/Users/flatironschool/blogs/3-speculative-markhov/csvs/speculative-{medium}.csv')
 
         df=df1.append(df2.append(df3))
 
@@ -58,11 +61,11 @@ class Necromancer():
     def shambler(cls,medium,words):
         import pickle
         if medium == 'title':
-            corpus=pickle.load( open("t_corpus.pkl",'rb'))
-            dictionary=pickle.load( open("t_dictionary.pkl",'rb'))
+            corpus=pickle.load( open("/Users/flatironschool/blogs/3-speculative-markhov/t_corpus.pkl",'rb'))
+            dictionary=pickle.load( open("/Users/flatironschool/blogs/3-speculative-markhov/t_dictionary.pkl",'rb'))
         else:
-            corpus=pickle.load( open("d_corpus.pkl",'rb'))
-            dictionary=pickle.load( open("d_dictionary.pkl",'rb'))
+            corpus=pickle.load( open("/Users/flatironschool/blogs/3-speculative-markhov/d_corpus.pkl",'rb'))
+            dictionary=pickle.load( open("/Users/flatironschool/blogs/3-speculative-markhov/d_dictionary.pkl",'rb'))
         n_words=words
         beginning = np.random.choice(corpus)
         
