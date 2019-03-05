10945ab5b548978a8148e5f706c79993409e8c16
Dan Izquierdo <dan.j.izquierdo@gmail.com>
2019-03-05 18:52:03 -0500

    Add comments

--- ScriptWhichMustNotBeNamed.py
+++ ScriptWhichMustNotBeNamed.py
@@ -8,11 +8,10 @@ import time
 from twython import Twython
 from raise_corpus import Necromancer
 
-print('Opening keys')
+# open keys stored locally
 with open('/Users/flatironschool/blogs/3-speculative-markhov/keys.txt') as f:
     content=f.readlines()
 keys= [line.strip() for line in content]
-print('Opened keys')
 # input API key, API secret key, Access token, Secret access token
 
 twitter = Twython(keys[0],
@@ -21,37 +20,45 @@ twitter = Twython(keys[0],
                   keys[3])
 
 def output_tweet(text,tweeted):
-	print('tweeting')
+"""
+Take given text and send it as a tweet. If tweeted is not 0 then the tweet will be sent as a reply to the most recent update for this profile.    
+"""
 	if tweeted==0:
 		twitter.update_status(status=text)
 	else:
 		last_tweet=twitter.get_home_timeline(count=1)
 		twitter.update_status(status=text,in_reply_to_status_id=last_tweet[0]['id'])
 
-# generate title and description
-print('Rising from the dead')
+# generate title and description from Markhov chain generator class
 tweet = Necromancer().shambler('title',6)
 tweet+= ': '+Necromancer().shambler('description',60)
-print('Risen')
+# initialize leftover text string
 dregs=''
+# initialize boolean to check if a tweet has already been sent
 tweeted=0
+# checks if a tweet is too long
 while len(tweet) > 280:
-    # if it is too long, try to cut off at a sentence
+    # if it is too long, try to cut off at the end of a sentence
     try:
         dregs= re.search(r"(.+?\.)(.*)", tweet).group(2)
         tweet= re.search(r"(.+?\.)(.*)", tweet).group(1)
         if len(tweet) >280:
-            # at least we tried, just take the first 140 characters
+            # at least we tried, just take the first 280 characters and
+            # store the rest
             dregs=tweet[280:]+dregs
             tweet=tweet[:280]
     except:
         if len(tweet) >280:
-            # at least we tried, just take the first 140 characters
+            # at least we tried, just take the first 280 characters
             dregs=tweet[280:]+dregs
             tweet=tweet[:280]
+    # send out the first part of the tweet
     output_tweet(tweet,tweeted)
+    # make sure to set that we already tweeted
     tweeted=1
     time.sleep(1)
+    # reset the text to only the part that has not been sent yet
     tweet=dregs
+# send out whatever text is left
 output_tweet(tweet,tweeted)
 os._exit(0)
--- raise_corpus.py
+++ raise_corpus.py
@@ -11,10 +11,18 @@ class Necromancer():
         pass
 
     def pair_up(self,corpus):
+    '''
+    Create generator to run through a body of text and collect pairs of words with the
+    word that follows them.
+    '''
         for word in range(len(corpus)-1):
             yield (corpus[word],corpus[word+1])
 
     def raise_corpus(self,medium):
+    '''
+    Read local text bodies taken from Goodreads and create the corpus used by the algorithm 
+    to generate Markhov chains.
+    '''
         df1=pd.read_csv(f'/Users/flatironschool/blogs/3-speculative-markhov/csvs/fantasy-{medium}.csv')
         df2=pd.read_csv(f'/Users/flatironschool/blogs/3-speculative-markhov/csvs/sci-fi-{medium}.csv')
         df3=pd.read_csv(f'/Users/flatironschool/blogs/3-speculative-markhov/csvs/speculative-{medium}.csv')
@@ -23,7 +31,7 @@ class Necromancer():
 
         corpus=df['0'].str.split()
         d_corpus=''
-
+        # clean up the text by replacing many unnecessary characters as well as odd concatenations 
         for medium in corpus:
             book=' '.join(medium).replace('/','').replace('\\','').replace('"','').replace('-','')
             book=re.sub(r'([a-z/.])([A-Z]{1})',r'\1 \2',book)
@@ -31,7 +39,7 @@ class Necromancer():
         d_corpus=d_corpus.split()
         dictionary={}
         pairs=self.pair_up(d_corpus)
-
+        # run through text and create pairs of words to construct dictionary of options for each word
         for prior,posterior in pairs:
             if prior in dictionary.keys():
                 dictionary[prior].append(posterior)
@@ -40,25 +48,37 @@ class Necromancer():
         return d_corpus, dictionary
 
     def shamble(self,d_corpus, dictionary,n_words):
+    '''
+    Given a body of text, dictionary of words in the text, and an amount of words: generate that number of
+    words.
+    '''
+        # choose a random starting point
         beginning = np.random.choice(d_corpus)
-
+        # might as well make sure it's uppercase
         while beginning.islower():
             beginning = np.random.choice(d_corpus)
-
+        # start the chain
         chain=[beginning]
-
+        # progress through the chain
         for i in range(n_words):
             next_word = np.random.choice(dictionary[chain[-1]])
+            # if the word ends a sentence, make sure the next word is capital
             if bool(re.search(r'.*[\.?!]$',chain[-1])):
                 while next_word.islower():
                     next_word = np.random.choice(dictionary[chain[-1]])
             chain.append(next_word)
+        # join the words in the chain together
         chain=' '.join(chain)
+        # cut off the generated text at the end of a sentence if possible
         chain=re.sub(r'(.*[.]).*',r'\1',chain)
         return chain
 
     @classmethod
     def shambler(cls,medium,words):
+    '''
+    Given local files with prebuilt corpora and dictionaries, generate text for a number of words 
+    for either the medium of titles or descriptions
+    '''
         import pickle
         if medium == 'title':
             corpus=pickle.load( open("/Users/flatironschool/blogs/3-speculative-markhov/t_corpus.pkl",'rb'))
@@ -67,19 +87,23 @@ class Necromancer():
             corpus=pickle.load( open("/Users/flatironschool/blogs/3-speculative-markhov/d_corpus.pkl",'rb'))
             dictionary=pickle.load( open("/Users/flatironschool/blogs/3-speculative-markhov/d_dictionary.pkl",'rb'))
         n_words=words
+        # choose a random starting point
         beginning = np.random.choice(corpus)
-        
+        # might as well make sure it's uppercase
         while beginning.islower():
             beginning = np.random.choice(corpus)
-    
+        # start the chain
         chain=[beginning]
-        
+        # progress through the chain
         for i in range(n_words):
             next_word = np.random.choice(dictionary[chain[-1]])
+            # if the word ends a sentence, make sure the next word is capital
             if bool(re.search(r'.*[\.?!]$',chain[-1])):
                 while next_word.islower():
                     next_word = np.random.choice(dictionary[chain[-1]])
             chain.append(next_word)
+        # join the words in the chain together
         chain=' '.join(chain)
+        # cut off the generated text at the end of a sentence if possible
         chain=re.sub(r'(.*[.]).*',r'\1',chain)
         return chain
